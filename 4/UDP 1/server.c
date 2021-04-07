1db604561d90ae7d93b362fc06bd60f0c6adc0ab
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-04-07 14:14:06 +0300

    UDP 1

--- 4/UDP 1/server.c
+++ 4/UDP 1/server.c
@@ -11,14 +11,14 @@ int main() {
 
     // create the server socket
     int server_socket;
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (server_socket == -1) {
         perror("Server socket creation failed!");
         exit(0);
     }
 
     // define the server address
-    struct sockaddr_in server_address;
+    struct sockaddr_in server_address, client_address;
     // use IPv4 addresses
     server_address.sin_family = AF_INET;
     // use port 9302
@@ -29,24 +29,23 @@ int main() {
 
     // bind the socket to our specified IP and port
     int b_code = bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-    if (b_code == -1) {
+    if (b_code < 0) {
         perror("Failed to bind socket to IP & port");
         exit(0);
     }
-    
-    // make the socket enter into listening state
-    listen(server_socket, 5);
 
-    // accept an incoming connection from the remote client
-    // and create an associated socket
-    int client_socket;
-    client_socket = accept(server_socket, NULL, NULL);
+    // receive data from client
+    char buf[256];
+    int size = sizeof(client_address);
+    recvfrom(server_socket, buf, 256, 0, (struct sockaddr *) &client_address, &size);
+    printf("Server received: %s\n", buf);
 
     // send data to the client
-    send(client_socket, server_message, sizeof(server_message), 6);
+    sendto(server_socket, server_message, sizeof(server_message), 0, (struct sockaddr*) &client_address, sizeof(client_address));
+    printf("Server sent: %s\n", server_message);
     
     // close the socket
-    close(client_socket);
+    close(server_socket);
     
     return 0;
 }
