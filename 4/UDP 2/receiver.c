b6b4cf0420547de0ba35ba5f9a08929f1b5dc40c
Jerim Kaura <50904889+jerimkaura@users.noreply.github.com>
2021-04-07 15:55:37 +0300

    variable names fix

--- 4/UDP 2/receiver.c
+++ 4/UDP 2/receiver.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<arpa/inet.h>
+#include<unistd.h>
 
 int  main (){
     system("clear");
@@ -18,19 +18,19 @@ int  main (){
         exit(0);
     }
 
-    struct sockaddr_in server_address, client_address;
+    struct sockaddr_in sender_address, receiver_address;
 
     // Set port and IP:
-    server_address.sin_family = AF_INET;
+    sender_address.sin_family = AF_INET;
 
     //port 
-    server_address.sin_port = htons(5000);
+    sender_address.sin_port = htons(5000);
 
     //ip address
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sender_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     // Bind to the set port and IP:
-    if(bind(receiver_descriptor, (struct sockaddr*)&server_address, sizeof(server_address)) < 0){
+    if(bind(receiver_descriptor, (struct sockaddr*)&sender_address, sizeof(server_address)) < 0){
         printf("Couldn't bind to the port\n");
         exit(0);
     }
@@ -41,7 +41,7 @@ int  main (){
         int size = sizeof(server_address);
 
         // receive message from sender
-        int len = recvfrom(receiver_descriptor, receiver_message, sizeof(receiver_message), 0, (struct sockaddr*)&client_address, &size);
+        int len = recvfrom(receiver_descriptor, receiver_message, sizeof(receiver_message), 0, (struct sockaddr*)&receiver_address, &size);
         receiver_message[len] = '\0';
 
         //terminate chat session upon receiving "end"
@@ -50,18 +50,18 @@ int  main (){
         }
 
         //output the message
-        printf("Message received : %s\n", receiver_message);
+        printf("\nParty 1 message: %s", receiver_message);
 
         // Get input from the user:
-        printf("Enter message: ");
+        printf("You:");
         fgets(sender_message, 2000, stdin);
         //scanf("%s", sender_message);
 
         //send respose to sender
-        sendto(receiver_descriptor, sender_message, strlen(sender_message), 0, (struct sockaddr*)&client_address, sizeof(client_address));
+        sendto(receiver_descriptor, sender_message, strlen(sender_message), 0, (struct sockaddr*)&receiver_address, sizeof(receiver_address));
         
         //terminate chat session upon sending "end"
-        if(strcmp(receiver_message[0], "end")==0){
+        if(strcmp(receiver_message, "end")==0){
             break;
         }
     }
