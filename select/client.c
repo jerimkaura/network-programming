64a8d031a36ce5bd1f7c2aed0dbc3fab279373d3
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-04-28 18:05:40 +0300

    prettier

--- select/client.c
+++ select/client.c
@@ -9,74 +9,72 @@
 #include <pthread.h>
 
 //Receiving a message
-void *recvmg(void *sock)
-{
-	int socket_fd = *((int *)sock);
-	char msg[500];
-	int len;
-    //receive unitl end of message
-	while((len = recv(socket_fd,msg,500,0)) > 0) {
-		msg[len] = '\0';
+void *recvmg(void *sock) {
+  int socket_fd = *((int *)sock);
+  char msg[500];
+  int len;
+  //receive unitl end of message
+  while ((len = recv(socket_fd, msg, 500, 0)) > 0) {
+    msg[len] = '\0';
 
-        //print received message
-		fputs(msg,stdout);
+    //print received message
+    fputs(msg, stdout);
 
-        //clear the message buffer
-		memset(msg,'\0',sizeof(msg)); 
-	}
+    //clear the message buffer
+    memset(msg, '\0', sizeof(msg));
+  }
 }
 
-int main(int argc, char *argv[])
-{
-	struct sockaddr_in socket_addr;
-	int my_socket;
-	int socket_fd;
-	int socket_addr_size;
-	int portno;
-	pthread_t send_thread,receive_thread;
-	char msg[500];
-	char username[100];
-	char res[600];
-	char ip[INET_ADDRSTRLEN];
-	int len;
+int main(int argc, char *argv[]) {
+  struct sockaddr_in socket_addr;
+  int my_socket;
+  int socket_fd;
+  int socket_addr_size;
+  int portno;
+  pthread_t send_thread, receive_thread;
+  char msg[500];
+  char username[100];
+  char res[600];
+  char ip[INET_ADDRSTRLEN];
+  int len;
 
-    //chekc argument count.
-	if(argc > 3) {
-		printf("too many arguments");
-		exit(1);
-	}
-	portno = atoi(argv[2]); //get the port number from argv[2]
-	strcpy(username,argv[1]); //copy name from argv[1] into the username variable
-	my_socket = socket(AF_INET,SOCK_STREAM,0); // create the socket
-	memset(socket_addr.sin_zero,'\0',sizeof(socket_addr.sin_zero));
+  //chekc argument count.
+  if (argc > 3) {
+    printf("too many arguments");
+    exit(1);
+  }
 
-	socket_addr.sin_family = AF_INET;
-	socket_addr.sin_port = htons(portno);
-	socket_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  portno = atoi(argv[2]);                      //get the port number from argv[2]
+  strcpy(username, argv[1]);                   //copy name from argv[1] into the username variable
+  my_socket = socket(AF_INET, SOCK_STREAM, 0); // create the socket
+  memset(socket_addr.sin_zero, '\0', sizeof(socket_addr.sin_zero));
 
-	if(connect(my_socket,(struct sockaddr *)&socket_addr,sizeof(socket_addr)) < 0) {
-		perror("connection not esatablished");
-		exit(1);
-	}
-	inet_ntop(AF_INET, (struct sockaddr *)&socket_addr, ip, INET_ADDRSTRLEN);
-	printf("connected to %s, start chatting\n",ip);
+  socket_addr.sin_family = AF_INET;
+  socket_addr.sin_port = htons(portno);
+  socket_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // creating the thread to receive the message
-	pthread_create(&receive_thread,NULL,recvmg,&my_socket);
+  if (connect(my_socket, (struct sockaddr *)&socket_addr, sizeof(socket_addr)) < 0) {
+    perror("connection not esatablished");
+    exit(1);
+  }
+  inet_ntop(AF_INET, (struct sockaddr *)&socket_addr, ip, INET_ADDRSTRLEN);
+  printf("connected to %s, start chatting\n", ip);
 
-	while(fgets(msg,500,stdin) > 0) { // get input from the client
-		strcpy(res,username);
-		strcat(res,": ");
-		strcat(res,msg);
-		len = write(my_socket,res,strlen(res));
-		if(len < 0) { // if the write function return 0, the nothing was written
-			perror("message not sent");
-			exit(1);
-		}
-		memset(msg,'\0',sizeof(msg));// clear msg
-		memset(res,'\0',sizeof(res));// clear res
-	}
-	pthread_join(receive_thread,NULL);
-	close(my_socket);
+  // creating the thread to receive the message
+  pthread_create(&receive_thread, NULL, recvmg, &my_socket);
 
+  while (fgets(msg, 500, stdin) > 0) { // get input from the client
+    strcpy(res, username);
+    strcat(res, ": ");
+    strcat(res, msg);
+    len = write(my_socket, res, strlen(res));
+    if (len < 0) { // if the write function return 0, the nothing was written
+      perror("message not sent");
+      exit(1);
+    }
+    memset(msg, '\0', sizeof(msg)); // clear msg
+    memset(res, '\0', sizeof(res)); // clear res
+  }
+  pthread_join(receive_thread, NULL);
+  close(my_socket);
 }
--- select/server.c
+++ select/server.c
@@ -11,48 +11,48 @@
 
 int clients[10]; // hold sockets to all clients
 
-int main(int argc,char *argv[]) {
-	struct sockaddr_in my_addr,their_addr;
-	int my_sock, their_sock;
-	socklen_t their_addr_size;
+int main(int argc, char *argv[]) {
+    struct sockaddr_in my_addr, their_addr;
+    int my_sock, their_sock;
+    socklen_t their_addr_size;
 
-	int portno;
+    int portno;
 
-    //max message length
-	char msg[500];
-	int len;
-	char ip[INET_ADDRSTRLEN];
-	if(argc > 2) {// two arguments maximum
-		printf("too many arguments");
-		exit(1);
-	}
-	portno = atoi(argv[1]);
+    // max message length
+    char msg[500];
+    int len;
+    char ip[INET_ADDRSTRLEN];
+    if (argc > 2) { // two arguments maximum
+        printf("too many arguments");
+        exit(1);
+    }
+    portno = atoi(argv[1]);
 
-    //create socket
-	my_sock = socket(AF_INET,SOCK_STREAM,0);
-	memset(my_addr.sin_zero,'\0',sizeof(my_addr.sin_zero));
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(portno);
-	my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	their_addr_size = sizeof(their_addr);
+    // create socket
+    my_sock = socket(AF_INET, SOCK_STREAM, 0);
+    memset(my_addr.sin_zero, '\0', sizeof(my_addr.sin_zero));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(portno);
+    my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    their_addr_size = sizeof(their_addr);
 
-    ///bind the socket to the address and port number
-	if(bind(my_sock,(struct sockaddr *)&my_addr,sizeof(my_addr)) != 0) {
-		perror("binding unsuccessful");
-		exit(1);
-	}
+    // bind the socket to the address and port number
+    if (bind(my_sock, (struct sockaddr *)&my_addr, sizeof(my_addr)) != 0) {
+        perror("binding unsuccessful");
+        exit(1);
+    }
 
-    //listen for connections from clients
-	if(listen(my_sock,10) != 0) {
-		perror("listening unsuccessful");
-		exit(1);
-	}
+    // listen for connections from clients
+    if (listen(my_sock, 10) != 0) {
+        perror("listening unsuccessful");
+        exit(1);
+    }
 
     fd_set current_sockets; // file descriptor sets (bitset)
     for (int i = 0; i < 10; i++)
         clients[i] = 0;
 
-	while (true) {
+    while (true) {
         FD_ZERO(&current_sockets); // clear fd set
         FD_SET(my_sock, &current_sockets);
         int maxfd = my_sock;
@@ -66,16 +66,16 @@ int main(int argc,char *argv[]) {
 
         if (select(maxfd + 1, &current_sockets, NULL, NULL, NULL) < 1) {
             perror("select error");
-            exit(EXIT_FAILURE); 
+            exit(EXIT_FAILURE);
         }
 
         if (FD_ISSET(my_sock, &current_sockets)) { // new connection
-            if ((their_sock = accept(my_sock,(struct sockaddr *)&their_addr,&their_addr_size)) < 0) {
+            if ((their_sock = accept(my_sock, (struct sockaddr *)&their_addr, &their_addr_size)) < 0) {
                 perror("accept unsuccessful");
                 exit(1);
             }
             inet_ntop(AF_INET, (struct sockaddr *)&their_addr, ip, INET_ADDRSTRLEN);
-            printf("%s connected\n",ip);
+            printf("%s connected\n", ip);
 
             for (int j = 0; j < 10; j++) {
                 if (clients[j] == 0) {
@@ -96,7 +96,7 @@ int main(int argc,char *argv[]) {
                         msg[len] = '\0';
                         for (int j = 0; j < 10; j++) {
                             if (clients[j] != 0 && clients[j] != sfd) {
-                                if (send(clients[j], msg, strlen(msg),0) < 0) {
+                                if (send(clients[j], msg, strlen(msg), 0) < 0) {
                                     perror("sending failure");
                                     continue;
                                 }
@@ -106,6 +106,6 @@ int main(int argc,char *argv[]) {
                 }
             }
         }
-	}
-	return 0;
+    }
+    return 0;
 }
