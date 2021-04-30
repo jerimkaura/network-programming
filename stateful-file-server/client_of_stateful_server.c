c8e497a01e69899ce8964b09183c1e9329d6d31b
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-04-30 15:22:38 +0300

    bug fix & prettier

--- stateful-file-server/client_of_stateful_server.c
+++ stateful-file-server/client_of_stateful_server.c
@@ -3,83 +3,79 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
+
 #define SIZE 1024
 
-//clients update the state information table each time they create aconnection with
-//the server 
+// clients update the state information table each time they create a connection with
+// the server 
 void store_state_info(int port, char *ip)
 {
 	int client_socket_counter = 1; // a unique identifier for each client in the state info table
-   FILE *fp;
-   fp = fopen("state_info.txt", "a+"); // a+ to ensure appending
-   fprintf(fp, "\nClient connection details:- \n");
-   fputs("Client socket number:- ", fp);
-   fprintf(fp, "%d \n", client_socket_counter);
-   fprintf(fp, "Client port number:- %d \n", port);
-   fprintf(fp, "Client ip address:- %s \n", &*ip);
-   fputs("\n",fp);
-   client_socket_counter = client_socket_counter + 1; //value is incremented for ease of numbering and works best in concurent servers
-   fclose(fp);
-
+	FILE *fp;
+	fp = fopen("state_info.txt", "a+"); // a+ to ensure appending
+	fprintf(fp, "\nClient connection details:- \n");
+	fputs("Client socket number:- ", fp);
+	fprintf(fp, "%d \n", client_socket_counter);
+	fprintf(fp, "Client port number:- %d \n", port);
+	fprintf(fp, "Client ip address:- %s \n", &*ip);
+	fputs("\n",fp);
+	client_socket_counter = client_socket_counter + 1; // value is incremented for ease of numbering and works best in concurent servers
+	fclose(fp);
 }
 
-//send file function
+// send file function
 void send_file(FILE *fp, int sockfd){
 	int n;
 	char data[SIZE] = {0};
-	while(fgets(data, SIZE, fp) != NULL) {
+	while (fgets(data, SIZE, fp) != NULL) {
 		// send the data to the server
 		if (send(sockfd, data, sizeof(data), 0) == -1) {
 			perror("Error in sending file.");
 			exit(1);
 		}
-		bzero(data, SIZE);// empty the data buffer
+		bzero(data, SIZE); // empty the data buffer
 	}
 }
 
-int main(){
-	//ip address
+int main() {
+	// ip address
 	char *ip = "127.0.0.1"; 
 
-	//port number
-	int port = 8080;
+	// port number
+	int connection, sockfd, port = 8080;
 
-	int connection;
-
-	int sockfd;
 	struct sockaddr_in server_addr;
 	FILE *fp;
 
-	//filename 
+	// filename 
 	char *filename = "file.txt";
 
-	//create the socket 
+	// create the socket 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0) {
+	if (sockfd < 0) {
 		perror("Error  creating the socket");
 		exit(1);
 	}
-	printf("Server socket created successfully.\n");
+	printf("Socket created successfully.\n");
 
-	//set the port and ip address
+	// set the port and ip address
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = port;
 	server_addr.sin_addr.s_addr = inet_addr(ip);
 
-	//execute the connect function
+	// execute the connect function
 	connection = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	if(connection == -1) {
+	if (connection == -1) {
 		perror("Error in socket");
 		exit(1);
 	}
-		printf("Connected to Server.\n");
+	printf("Connected to Server.\n");
 
-	//store state information that will help in future connections
-	void store_state_info(int port, char *ip);
+	// store state information that will help in future connections
+	store_state_info(port, ip);
 
-
-	//open the filename specifed in read mode, the data read from the file is stored in a the variable
-	//fp
+	// open the filename specifed in read mode, the data read from the file is stored in a the variable
+	// fp
 	fp = fopen(filename, "r");
 	if (fp == NULL) {
 		perror("Error in reading file.");
@@ -90,8 +86,8 @@ int main(){
 	send_file(fp, sockfd);
 	printf("File data sent successfully.\n");
 	printf("Closing the connection.\n");
+
 	// close the connection
 	close(sockfd);
 	return 0;
-	
 }
--- stateful-file-server/stateful_server.c
+++ stateful-file-server/stateful_server.c
@@ -2,57 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+
 #define SIZE 1024
 
+void retrieve_state_info(void) {
+	char ch;
+	FILE *fp;
 
-void retrieve_state_info(void)
-{
-    char ch;
-   FILE *fp;
+	fp = fopen("state_info.txt", "r"); // read state info
 
-   fp = fopen("state_info.txt", "r"); // read state info
+	/* if (fp == NULL)
+	{
+		perror("Error while opening the file.\n");
+		exit(EXIT_FAILURE);
+	}*/
 
-  /* if (fp == NULL)
-   {
-      perror("Error while opening the file.\n");
-      exit(EXIT_FAILURE);
-   }*/
+	printf("The state information is as follows:- ");
 
-   printf("The state information is as follows:- ");
+	while ((ch = fgetc(fp)) != EOF)
+		printf("%c", ch);
 
-   while((ch = fgetc(fp)) != EOF)
-      printf("%c", ch);
-
-   fclose(fp);
-
+	fclose(fp);
 }
 
-void write_file(int sockfd){
+void write_file(int sockfd) {
 	int n;
 	FILE *fp;
 	char *filename = "recv.txt";
 	char buffer[SIZE];
 
 	fp = fopen(filename, "w");
-	printf("Client Sent \n");
+	printf("Client Sent: ");
 	while (1) {
-		//receive the data from the client and store in te buffer
+		// receive the data from the client and store in the buffer
 		n = recv(sockfd, buffer, SIZE, 0);
 		if (n <= 0){
 			break;
 			return;
 		}
-		printf("%s",buffer);
+		printf("%s", buffer);
 
-		//save the data to the file specified
+		// save the data to the file specified
 		fprintf(fp, "%s", buffer);
-		bzero(buffer, SIZE);//empty the buffer 
+		bzero(buffer, SIZE); // empty the buffer 
 	}
-	return;
+	printf("\n");
+	fclose(fp);
 }
 
 
-void main(){
+void main() {
 	char *ip = "127.0.0.1"; // ip adsress
 	int port = 8080; //port number
 	int conn; //connection variable
@@ -62,7 +61,7 @@ void main(){
 	socklen_t addr_size;
 	char buffer[SIZE];
 
-	//create the socket
+	// create the socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0) {
 		perror("Error in socket");
@@ -70,12 +69,12 @@ void main(){
 	}
 	printf("Server socket created successfully.\n");
 
-	//specify the port and address
+	// specify the port and address
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = port;
 	server_addr.sin_addr.s_addr = inet_addr(ip);
 
-	//bind the socket to the server address
+	// bind the socket to the server address
 	conn = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	if(conn < 0) {
 		perror("Error in bind");
@@ -83,25 +82,25 @@ void main(){
 	}
 	printf("Binding successfull.\n");
 
-	//retrieve state information 
+	// retrieve state information 
 	retrieve_state_info();
 
 	// listen for connection request from the clients
-	if(listen(sockfd, 10) == 0){
+	if (listen(sockfd, 10) == 0){
 		printf("Listening....\n");
-	}else{
+	} else {
 		perror("Error in listening");
 		exit(1);
 	}
 
 	addr_size = sizeof(new_addr);// address size 
 	while (1) {
-		//accept the connection from the cleint
+		// accept the connection from the cleint
 		new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-		//call the write function
+		// call the write function
 		write_file(new_sock);
 	}
 	printf("Data written in the file successfully.\n");
 
-	
+	return 0;
 }
